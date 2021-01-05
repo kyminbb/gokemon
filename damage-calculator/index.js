aab578f05a350ad5a2e4c15cde6c4e7994e3677d
Kyungmin Kim <kyminbb@Kyungmins-MacBook-Pro.local>
2021-01-05 21:40:01 +0900

    Implement simple name query

--- damage-calculator/index.js
+++ damage-calculator/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const http = require('http');
 const ws = require('ws');
 
+const calc = require('@smogon/calc');
+const gen = calc.Generations.get(8);
+const mode = { NAME1: 0, NAME2: 1, MOVE: 2 }
+
 const app = express();
 
 const server = http.createServer(app);
@@ -9,13 +13,38 @@ const server = http.createServer(app);
 const wss = new ws.Server({ server });
 
 wss.on('connection', (socket) => {
+    let curMode = mode.NAME1;
+    let pokemon1, pokemon2, move;
+    socket.send('What is the name of your Pokemon?');
     socket.on('message', (data) => {
-        console.log(`Recieved ${data}`);
+        switch (curMode) {
+            case mode.NAME1: 
+                console.log(`Pokemon 1: ${data}`);
+                pokemon1 = new calc.Pokemon(gen, data);
+                curMode = mode.NAME2;
+                socket.send('What is the name of the enemy Pokemon?');
+                break;
+            case mode.NAME2: 
+                console.log(`Pokemon 2: ${data}`);
+                pokemon2 = new calc.Pokemon(gen, data);
+                curMode = mode.MOVE;
+                socket.send('Which skill do you use?');
+                break;
+            case mode.MOVE:
+                console.log(`Move: ${data}`);
+                move = new calc.Move(gen, data);
+                const result = calc.calculate(gen, pokemon1, pokemon2, move);
+                socket.send(`Possible damages: ${result.damage}`);
+                socket.close();
+                break;
+            default:
+                return;
+        }
     });
+
     socket.on('close', (code) => {
         console.log(`Socket closed! (CODE ${code})`);
     });
-    socket.send("Hello from the server!");
 });
 
 // Start the server
