03d312ebb94dcd623279b0e28e1814dffa20842b
Kyungmin Kim <kyminbb@Kyungmins-MacBook-Pro.local>
2021-01-15 21:00:29 +0900

    Calculate the average damages for all 4 moves

--- damage-calculator/calc.js
+++ damage-calculator/calc.js
@@ -6,20 +6,27 @@ const getPokemon = ({ name, ...params }) => {
     return new smogon.Pokemon(gen, name, options);
 };
 
-const getMove = data => new smogon.Move(gen, data);
+const getMoves = data => data.map(moveName => new smogon.Move(gen, moveName));
 
 const getField = data => new smogon.Field();
 
-const avg = arr => {
+const average = arr => {
     if (arr == 0 || arr.length == 0) return 0;
     const sum = arr.reduce((x, y) => x + y, 0);
     return sum / arr.length;
 }
 
+const getAverageDamages = (attacker, defender, moves) => {
+    const res = {};
+    moves.forEach(move => {
+        res[move.name] = average(smogon.calculate(gen, attacker, defender, move).damage);
+    });
+    return res;
+};
+
 exports.calcDamage = ({ from, to }) => {
     const attacker = getPokemon(from);
     const defender = getPokemon(to);
-    const move = getMove(from.moves[0]);
-    const result = smogon.calculate(gen, attacker, defender, move);
-    return avg(result.damage);
+    const moves = getMoves(from.moves);
+    return getAverageDamages(attacker, defender, moves);
 };
